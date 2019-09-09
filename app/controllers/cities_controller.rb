e326d525228633a0068c4544913654c8164cb17b
JulienTexier <45203192+JulienTexier@users.noreply.github.com>
2019-09-09 11:39:13 +0200

    Update cities_controller.rb

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,28 +1,20 @@
 class CitiesController < ApplicationController
   before_action :set_city, only: [:show, :edit, :update, :destroy]
 
-  # GET /cities
-  # GET /cities.json
   def index
     @cities = City.all
   end
 
-  # GET /cities/1
-  # GET /cities/1.json
   def show
   end
 
-  # GET /cities/new
   def new
     @city = City.new
   end
 
-  # GET /cities/1/edit
   def edit
   end
 
-  # POST /cities
-  # POST /cities.json
   def create
     @city = City.new(city_params)
 
@@ -37,8 +29,6 @@ class CitiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /cities/1
-  # PATCH/PUT /cities/1.json
   def update
     respond_to do |format|
       if @city.update(city_params)
@@ -51,8 +41,6 @@ class CitiesController < ApplicationController
     end
   end
 
-  # DELETE /cities/1
-  # DELETE /cities/1.json
   def destroy
     @city.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class CitiesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_city
       @city = City.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def city_params
       params.require(:city).permit(:name, :zip_code)
     end
