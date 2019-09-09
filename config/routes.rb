ff4e4205b2e3df00c209b33471fe9110177d4bee
PaulMouchel <paulmouchel@live.fr>
2019-09-09 14:25:08 +0200

    changes routes for favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
   resources :ratings
-  resources :favorites
 	root 'cities#index'
   resources :cities do
   	resources :places
   end
   devise_for :users 
-  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update, :destroy] do
+  	resources :favorites
+  end
 
 end
