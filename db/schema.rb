4e0edc82669beefd9ad4a9241135207292a92ed6
PaulMouchel <paulmouchel@live.fr>
2019-09-09 17:05:53 +0200

    fix show Place issues

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2019_09_09_131711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -35,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_131711) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "attendances", force: :cascade do |t|
     t.bigint "user_id"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2019_09_09_131711) do
     t.datetime "updated_at", null: false
     t.index ["place_id"], name: "index_attendances_on_place_id"
     t.index ["user_id"], name: "index_attendances_on_user_id"
-
   end
 
   create_table "cities", force: :cascade do |t|
