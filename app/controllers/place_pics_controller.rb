ba7949764fad790a9e952437cfb4b91b324bab5a
PaulMouchel <52449717+PaulMouchel@users.noreply.github.com>
2019-09-09 16:56:12 +0200

    Update place_pics_controller.rb

--- app/controllers/place_pics_controller.rb
+++ app/controllers/place_pics_controller.rb
@@ -1,7 +1,8 @@
 class PlacePicsController < ApplicationController
   def create
+    @city = City.find(params[:city_id])
     @place = Place.find(params[:place_id])
     @place.place_pic.attach(params[:place_pic])
-    redirect_to(place_path(@place))
+    redirect_to(city_place_path(@city, @place))
   end
 end
