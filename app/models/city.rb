a5333ad51d4a68e64822004c6dd9f367767d29f2
PaulMouchel <paulmouchel@live.fr>
2019-09-09 11:23:37 +0200

    model City

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,11 @@
 class City < ApplicationRecord
+	validates name,
+		presence: true
+	validates :zip_code, 
+  	presence: true, 
+    uniqueness: true,
+    format: { with: /\A(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}\z/, message: "Please enter a valid french zip code" }
+
+	has_many: users
+	has_many: places, dependant: destroy
 end
