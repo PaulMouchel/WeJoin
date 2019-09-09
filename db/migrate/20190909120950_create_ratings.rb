48fb37ef90d853459aafd5b9090ef5a9cff08e52
PaulMouchel <paulmouchel@live.fr>
2019-09-09 14:14:15 +0200

    migration done

--- db/migrate/20190909120950_create_ratings.rb
+++ db/migrate/20190909120950_create_ratings.rb
@@ -1,6 +1,8 @@
 class CreateRatings < ActiveRecord::Migration[5.2]
   def change
     create_table :ratings do |t|
+    	t.belongs_to :user, index: true
+    	t.belongs_to :place, index: true
       t.integer :stars
 
       t.timestamps
