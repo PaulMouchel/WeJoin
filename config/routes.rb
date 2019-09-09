65bf922bbf27522e1cfad197f656ae6be38308c3
PaulMouchel <paulmouchel@live.fr>
2019-09-09 16:53:56 +0200

    remove route show on cities

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :tags
   resources :ratings
 	root 'cities#index'
-  resources :cities do
+  resources :cities , except: [:show] do
   	resources :places
   end
   devise_for :users 
