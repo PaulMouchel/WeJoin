9429f70b4cb3c0c083e4c5c825869713e376f5fb
Samkaa987 <deneubourg.samuel@gmail.com>
2019-09-09 16:10:52 +0200

    add profile + edit profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_120950) do
+ActiveRecord::Schema.define(version: 2019_09_09_123859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2019_09_09_120950) do
     t.datetime "updated_at", null: false
     t.index ["place_id"], name: "index_ratings_on_place_id"
     t.index ["user_id"], name: "index_ratings_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
