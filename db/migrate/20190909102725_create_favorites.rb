7255512ee879a82db3c198a7370682820497dedd
PaulMouchel <paulmouchel@live.fr>
2019-09-09 12:36:11 +0200

    change migration

--- db/migrate/20190909102725_create_favorites.rb
+++ db/migrate/20190909102725_create_favorites.rb
@@ -1,7 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-
+    	t.belongs_to :user, index: true
+    	t.belongs_to :place, index: true
       t.timestamps
     end
   end
