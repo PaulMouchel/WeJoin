ba70f9ace51ea28409b9a30a3bbe44958a74b235
JulienTexier <juu.texier@gmail.com>
2019-09-09 12:45:30 +0200

    adding attributes to users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :create, :update]
+
+  def show
+    @user = User.all.find(params[:id])
+  end
+  
+
+  
+  def update
+    current_user.update(user_params)
+      if current_user.save
+        flash[:success] = 'You account was successfully updated.'
+        redirect_to user_path(current_user.id)
+      else
+        flash.now[:error] = @order.errors.full_messages.to_sentence
+        render :edit
+      end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :description, :age)
+    end
+end
