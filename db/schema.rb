148fe1da2fde9e29bfccab866426ff9c122ec42f
PaulMouchel <paulmouchel@live.fr>
2019-09-09 11:58:00 +0200

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_091459) do
+ActiveRecord::Schema.define(version: 2019_09_09_095150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 2019_09_09_091459) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.string "zip_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.text "description"
+    t.float "coffee_price"
+    t.float "tea_price"
+    t.float "beer_price"
+    t.string "wifi_password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
