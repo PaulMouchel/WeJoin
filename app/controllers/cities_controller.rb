ed8a3ced8c066e3e607d40e49f05c37e340bc116
PaulMouchel <52449717+PaulMouchel@users.noreply.github.com>
2019-09-10 16:09:27 +0200

    Update cities_controller.rb

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,29 +1,5 @@
 class CitiesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :redirect_to_root, except: [:index, :show]
-
-
   def index
     @cities = City.all
   end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  private
-    def redirect_to_root
-      redirect_to cities_path
-    end
 end
