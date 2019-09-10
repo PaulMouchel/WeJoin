e6d30112bba4acacb66b9ba5b588af67c6b0eccc
PaulMouchel <paulmouchel@live.fr>
2019-09-10 17:45:56 +0200

    routing spec

--- spec/routing/places_routing_spec.rb
+++ spec/routing/places_routing_spec.rb
@@ -3,36 +3,36 @@ require "rails_helper"
 RSpec.describe PlacesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/places").to route_to("places#index")
+      expect(:get => "/cities/5/places").to route_to("places#index", :city_id => "5")
     end
 
     it "routes to #new" do
-      expect(:get => "/places/new").to route_to("places#new")
+      expect(:get => "/cities/5/places/new").to route_to("places#new", :city_id => "5")
     end
 
     it "routes to #show" do
-      expect(:get => "/places/1").to route_to("places#show", :id => "1")
+      expect(:get => "/cities/5/places/1").to route_to("places#show", :id => "1", :city_id => "5")
     end
 
     it "routes to #edit" do
-      expect(:get => "/places/1/edit").to route_to("places#edit", :id => "1")
+      expect(:get => "/cities/5/places/1/edit").to route_to("places#edit", :id => "1", :city_id => "5")
     end
 
 
     it "routes to #create" do
-      expect(:post => "/places").to route_to("places#create")
+      expect(:post => "/cities/5/places").to route_to("places#create", :city_id => "5")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/places/1").to route_to("places#update", :id => "1")
+      expect(:put => "/cities/5/places/1").to route_to("places#update", :id => "1", :city_id => "5")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/places/1").to route_to("places#update", :id => "1")
+      expect(:patch => "/cities/5/places/1").to route_to("places#update", :id => "1", :city_id => "5")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/places/1").to route_to("places#destroy", :id => "1")
+      expect(:delete => "/cities/5/places/1").to route_to("places#destroy", :id => "1", :city_id => "5")
     end
   end
 end
