49440ab4745801a1e65b87a504387248f3e31b00
PaulMouchel <paulmouchel@live.fr>
2019-09-11 14:39:19 +0200

    controller and views for admin tags

---
+++ app/controllers/admin/tags_controller.rb
@@ -0,0 +1,65 @@
+class TagsController < AdminController
+  before_action :set_tag, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def edit
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+
+    respond_to do |format|
+      if @tag.save
+        format.html { redirect_to @tag 
+        flash[:success] = 'Tag was successfully created.' }
+        format.json { render :show, status: :created, location: @tag }
+      else
+        format.html { flash.now[:error] = @tag.errors.full_messages.to_sentence
+          render :new }
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @tag.update(tag_params)
+        format.html { redirect_to @tag 
+        flash[:success] = 'Tag was successfully updated.' }
+        format.json { render :show, status: :ok, location: @tag }
+      else
+        format.html { flash.now[:error] = @tag.errors.full_messages.to_sentence
+          render :edit }
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @tag.destroy
+    respond_to do |format|
+      format.html { redirect_to tags_url 
+      flash[:success] = 'Tag was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_tag
+      @tag = Tag.find(params[:id])
+    end
+
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
+end
