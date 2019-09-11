0bd4ca8d55c718f5d97367bf88cb1114559a29aa
JulienTexier <juu.texier@gmail.com>
2019-09-11 16:13:44 +0200

    adding deleting option on photos

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :cities, except: [:show] do
-      resources :city_pics
-      resources :places do 
-        resources :place_pics
-      end
+      resources :city_pics, only: [:create, :destroy]
+      resources :places
     end
     resources :users do
-      resources :user_pics, only: [:create]
-      resources :favorites, except: [:show]
+      resources :user_pics, only: [:create, :destroy]
+      resources :favorites, except: [:show, :destroy]
       resources :attendances
     end
   end
@@ -17,14 +15,14 @@ Rails.application.routes.draw do
   resources :tags
 	root 'cities#index'
   resources :cities, only: [:index] do
-    resources :city_pics, only: [:create]
+    resources :city_pics, only: [:create, :destroy]
     resources :places, except: [:edit, :update, :destroy] do 
-      resources :place_pics, only: [:create]
+      resources :place_pics, only: [:create, :destroy]
     end
   end
   devise_for :users 
   resources :users, only: [:show, :edit, :update, :destroy] do
-    resources :user_pics, only: [:create]
+    resources :user_pics, only: [:create, :destroy]
   	resources :favorites, except: [:show]
   	resources :attendances
   	resources :ratings
