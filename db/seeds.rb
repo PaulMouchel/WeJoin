eb71e186f41002e0a4e8c4e5643482b6318ed470
Samkaa987 <deneubourg.samuel@gmail.com>
2019-09-13 11:48:36 +0200

    modified seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,20 @@
-rennes = City.find_by(name:"Rennes")
+rennes = City.find_by(name: "Rennes")
 if rennes == nil
-	rennes = City.create(name: "Rennes")
+	rennes = City.create!(name: "Rennes")
 end
 
 name = "Loco Loca"
 if rennes.places.find_by(name:name) == nil
 	address = "17 Rue Comté de Lanjuinais, 35000 Rennes"
 	description = "Espace contemporain stylé autour de la culture latine avec coffee-shop, bar à tapas, jardin et restaurant."
-	rennes.places.create(name:name, address:address, description:description, coffee_price:1.6, tea_price:3.5, beer_price:3.2)
+	wifi_identification = "LOCO LOCA"
+	rennes.places.create!(name:name, address:address, description:description, coffee_price:1.6, tea_price:3.5, beer_price:3.2, wifi_identification:wifi_identification)
 end
 
 name = "Café des champs libres"
 if rennes.places.find_by(name:name) == nil
 	address = "10 Cours des Alliés, 35000 Rennes"
 	description = ""
-	rennes.places.create(name:name, address:address, description:description, coffee_price:1.5)
+	wifi_identification = "Champs Libres"
+	rennes.places.create!(name:name, address:address, description:description, coffee_price:1.5, tea_price:3.5, beer_price:3.4, wifi_identification:wifi_identification)
 end
