8007a258903d122de2847f0d63a51214b4fdb92d
Samkaa987 <deneubourg.samuel@gmail.com>
2019-09-13 17:53:36 +0200

    delete rating wifi quality because problem

--- app/models/rating_wifi_quality.rb
+++
@@ -1,6 +0,0 @@
-class RatingWifiQuality < ApplicationRecord
-	validates :wifi_qualities, inclusion: 1..5, presence: true
-
-	belongs_to :user
-	belongs_to :place
-end
--- spec/controllers/rating_wifi_qualities_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe RatingWifiQualitiesController, type: :controller do
-
-end
