4ca19e6c5a45b0d9c9cb741caa7c567dabee05b8
PaulMouchel <paulmouchel@live.fr>
2019-09-14 18:49:47 +0200

    AJAX for attendances inside calendar

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,14 +1,14 @@
 class AttendancesController < ApplicationController
-	before_action :set_place, only: [:create]
+	before_action :set_place, only: [:create, :destroy]
   before_action :set_attendance, only: [:destroy]
 
   def index
-    @attendances = current_user.attendances
+  	@attendances = current_user.attendances
   end
 
   def create
     @attendance = Attendance.new(attendance_params)
-
+    @attendances = @place.attendances
     respond_to do |format|
       if @attendance.save
         format.html { redirect_back(fallback_location: root_path)
@@ -25,10 +25,12 @@ class AttendancesController < ApplicationController
 
   def destroy
     @attendance.destroy
+    @attendances = @place.attendances
+    @my_attendances = current_user.attendances
     respond_to do |format|
       format.html { redirect_back(fallback_location: root_path)
       flash[:success] = 'Attendance was successfully destroyed.' }
-      format.json { head :no_content }
+      format.js { }
     end
   end
 
@@ -36,6 +38,7 @@ class AttendancesController < ApplicationController
   	def set_place
       @place = Place.find(params[:place_id])
     end
+
     def set_attendance
       @attendance = Attendance.find(params[:id])
     end
