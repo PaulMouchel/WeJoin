4f768a86a10148810f0fa501ba7a52fbf4d48ed0
JulienTexier <juu.texier@gmail.com>
2019-09-17 14:04:44 +0200

    adding coordinates to cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_145708) do
+ActiveRecord::Schema.define(version: 2019_09_17_115640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2019_09_16_145708) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "favorites", force: :cascade do |t|
