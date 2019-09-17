b045df1e08ff1ce49624ef8c64461e9da1ceee1a
JulienTexier <juu.texier@gmail.com>
2019-09-17 11:16:58 +0200

    spec for user model fixing age

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     last_name { Faker::Name.last_name }
     description { Faker::TvShows::Simpsons.quote }
     is_admin { false }
-    birth_date { Faker::Date.birthday(min_age: 18, max_age: 75) }
+    birth_date { Faker::Date.birthday(min_age: 1, max_age: 110) }
     profession { Faker::Job.title }
     user_pic { Faker::Company.logo }
   end
