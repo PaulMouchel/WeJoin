f2c2171f7db85f389e1e5ca3f58aaf3d8256d095
JulienTexier <juu.texier@gmail.com>
2019-09-17 16:59:28 +0200

    adding maps & coordinates to places

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_115640) do
+ActiveRecord::Schema.define(version: 2019_09_17_141730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,8 @@ ActiveRecord::Schema.define(version: 2019_09_17_115640) do
     t.boolean "validated", default: false
     t.boolean "reviewed", default: false
     t.string "wifi_identification"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["city_id"], name: "index_places_on_city_id"
   end
 
