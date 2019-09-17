33dacc24d370b593ab5e9cf8051a27d688a334c9
PaulMouchel <paulmouchel@live.fr>
2019-09-17 20:15:22 +0200

    add api_key for geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   use_https: false,           # use HTTPS for lookup requests? (if supported)
   http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: nil,               # API key for geocoding service
+  api_key: 'pk.eyJ1IjoianVsaWVudGV4aWVyIiwiYSI6ImNrMG53cjQ4ZDA0MzgzbXJ1OHBraWU1emcifQ.JVgZSraZTxwT5cA0sGlGbA',               # API key for geocoding service
   cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
