1ce12a90eb396a7be199ca553eca43f288865ad7
PaulMouchel <paulmouchel@live.fr>
2019-09-19 15:16:43 +0200

    user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
-    it { should validate_uniqueness_of(:email)}
   end
 
   context "public instance methods" do
