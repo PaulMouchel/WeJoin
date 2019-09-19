2e880cddf9be0505ce79223481129253971f98e9
PaulMouchel <paulmouchel@live.fr>
2019-09-19 17:17:56 +0200

    no attendances in the past

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,6 @@
 class Attendance < ApplicationRecord
 	validates :date, presence: true
+	validate :in_the_future?
 
 	belongs_to :user
 	belongs_to :place
@@ -11,5 +12,14 @@ class Attendance < ApplicationRecord
   def users
   	attendances = Attendance.where(date: self.date, place: self.place)
   	return attendances.map{|attendance|attendance.user}
+  end
+
+  private
+
+  def in_the_future?
+  	#return self.start_date > Time.now
+  	if self.date.present? && self.date < 1.day.ago
+        errors.add(:date, "Pas de participation dans le passé !")
+    end
   end
 end
