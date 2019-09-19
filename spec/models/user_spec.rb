73278bf59141d38d87865b7c60ef61497f79a433
PaulMouchel <paulmouchel@live.fr>
2019-09-19 12:21:06 +0200

    place model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_uniqueness_of(:email)}
-    it { should validate_format_of(/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/) }
   end
 
   context "public instance methods" do
